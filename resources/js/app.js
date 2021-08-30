8db09b67c33877fbac7058d5876a21d8caf620a5
gaetk <gaetankulemfuka08@gmail.com>
2021-08-30 14:19:27 +0200

    Migrate Database

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 require('./bootstrap');
-import "admin-lite/plugins/jquery/jquery";
-import "admin-lite/plugins/bootstrap/js/bootstrap.bundle";
-import "admin-lite/dist/js/adminlte";
+
+import "admin-lte/plugins/jquery/jquery";
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle";
+import "admin-lte/dist/js/adminlte";
